86f4c07e1869a0ed62b48c96d1a871897e2b4cbc
GallickGun <heraditya22@gmail.com>
2022-06-23 15:14:00 +0700

    module object

---
+++ scripts/module-object.js
@@ -0,0 +1,13 @@
+export const firstName = "Gallick";
+export const lastName = "Gun";
+
+export function hello() {
+    console.info(`Hello from GallickGun`);
+}
+
+export class Person {
+
+    constructor(){
+        this.name = "GallickGuns";
+    }
+}
